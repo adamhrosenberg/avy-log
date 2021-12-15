ed5d33a091d2510314dc43818a6d66b3bb65425c
Adam Rosenberg <arosenberg@mastercontrol.com>
2021-12-14 19:28:24 -0700

    adding in view controllers for the app

---
+++ avy-log/avy-log/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avy-log
+//
+//  Created by Adam Rosenberg on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
