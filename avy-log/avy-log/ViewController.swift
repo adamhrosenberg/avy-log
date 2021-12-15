922f7def6f79505124f87da70eb885c2504c06ce
Adam Rosenberg <arosenberg@mastercontrol.com>
2021-12-14 19:49:52 -0700

    adding in table view hardcoded

--- avy-log/avy-log/ViewController.swift
+++ avy-log/avy-log/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "prototypeCell", for: indexPath);
+        cell.textLabel?.text = "Log";
+        return cell;
+    }
+    
 
+    @IBOutlet weak var logsListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logsListTableView.delegate = self;
+        logsListTableView.dataSource = self;
+        
+        
     }
 
 
